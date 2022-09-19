208a5eac0baf569f57debf9c86e6faff7750efe2
Saeed Heydari <saeedhpro@gmail.com>
2022-09-20 01:03:35 +0430

    Add Vip Schedule

--- domain/models/VipScheduleModel.go
+++ domain/models/VipScheduleModel.go
@@ -5,6 +5,8 @@ import "time"
 type VipScheduleModel struct {
 	ID             uint64             `json:"id" gorm:"primarykey"`
 	Count          uint               `json:"count" gorm:"count"`
+	Site           int                `json:"site" gorm:"site"`
+	App            int                `json:"app" gorm:"app"`
 	StartAt        *time.Time         `json:"start_at" gorm:"start_at"`
 	EndAt          *time.Time         `json:"end_at" gorm:"end_at"`
 	OrganizationID uint64             `json:"organization_id" gorm:"organization_id"`
